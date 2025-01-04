62fadaa609ab165110736064513bbc612686d408
ankur1812 <ankur.anmol1992@gmail.com>
2025-01-04 23:07:24 +0530

    code refactor, move logic to broadcast.dart screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,110 +1,23 @@
-import 'dart:io'; // For Process class to execute shell commands
+// import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:network_info_plus/network_info_plus.dart';
-import 'package:qr_flutter/qr_flutter.dart';
+import 'screens/broadcast.dart';
 
 void main() {
-  runApp(const BroadcastIPApp());
+  runApp(const MyApp());
 }
 
-class BroadcastIPApp extends StatefulWidget {
-  const BroadcastIPApp({Key? key}) : super(key: key);
-
-  @override
-  _BroadcastIPAppState createState() => _BroadcastIPAppState();
-}
-
-class _BroadcastIPAppState extends State<BroadcastIPApp> {
-  String? _localIP = "Fetching...";
-  final NetworkInfo _networkInfo = NetworkInfo(); // Initialize NetworkInfo
-
-  @override
-  void initState() {
-    super.initState();
-    _getLocalIP(); // Fetch the local IP address on startup
-  }
-
-  Future<void> _getLocalIP() async {
-    try {
-      // Attempt to get the Wi-Fi IP via network_info_plus first
-      String? wifiIP = await _networkInfo.getWifiIP();
-
-      if (wifiIP != null) {
-        setState(() {
-          _localIP = wifiIP; // Use network_info_plus if available
-        });
-      } else {
-        // If wifiIP is null, fallback to platform-specific logic
-        String? ipAddress = await _getPlatformSpecificIP();
-        setState(() {
-          _localIP = ipAddress ?? "Could not fetch local IP.";
-        });
-      }
-    } catch (e) {
-      setState(() {
-        _localIP = "Error: $e";
-      });
-    }
-  }
-
-  // Platform-specific logic to get the IP address
-  Future<String?> _getPlatformSpecificIP() async {
-    try {
-      if (Platform.isLinux || Platform.isMacOS) {
-        // Use ifconfig or ip command on Unix-based systems
-        var result = await Process.run('ifconfig', []);
-        if (result.exitCode == 0) {
-          // Extract inet address (IPv4) using regex
-          RegExp inetRegex = RegExp(r'inet (\d+\.\d+\.\d+\.\d+)');
-          var match = inetRegex.firstMatch(result.stdout);
-          if (match != null) return match.group(1);
-        }
-      } else if (Platform.isWindows) {
-        // Use ipconfig on Windows
-        var result = await Process.run('ipconfig', []);
-        if (result.exitCode == 0) {
-          // Extract IPv4 address using regex
-          RegExp ipv4Regex = RegExp(r'IPv4 Address[^\d]*(\d+\.\d+\.\d+\.\d+)');
-          var match = ipv4Regex.firstMatch(result.stdout);
-          if (match != null) return match.group(1);
-        }
-      }
-    } catch (e) {
-      print("Error fetching IP address: $e");
-    }
-    return null;
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Local IP Finder')),
-        body: SingleChildScrollView(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Divider(),
-              Container(
-                margin: const EdgeInsets.only(bottom: 16.0),
-                child: Text(
-                  'Your Local IP is: \n$_localIP:3000',
-                  style: const TextStyle(fontSize: 24),
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(bottom: 16.0),
-                child: QrImageView(
-                  data: '$_localIP:3000',
-                  version: QrVersions.auto,
-                  size: 200.0,
-                ),
-              ),
-            ],
-          ),
-        )
+      title: 'Find My IP',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent),
+        useMaterial3: true,
       ),
+      home: BroadcastScreen(),
     );
   }
 }
---
+++ lib/screens/broadcast.dart
@@ -0,0 +1,104 @@
+import 'dart:io'; // For Process class to execute shell commands
+import 'package:flutter/material.dart';
+import 'package:network_info_plus/network_info_plus.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+
+class BroadcastScreen extends StatefulWidget {
+  const BroadcastScreen({Key? key}) : super(key: key);
+
+  @override
+  _BroadcastScreenState createState() => _BroadcastScreenState();
+}
+
+class _BroadcastScreenState extends State<BroadcastScreen> {
+  String? _localIP = "Fetching...";
+  final NetworkInfo _networkInfo = NetworkInfo(); // Initialize NetworkInfo
+
+  @override
+  void initState() {
+    super.initState();
+    _getLocalIP();
+  }
+
+  Future<void> _getLocalIP() async {
+    try {
+      // Attempt to get the Wi-Fi IP via network_info_plus first
+      String? wifiIP = await _networkInfo.getWifiIP();
+
+      if (wifiIP != null) {
+        setState(() {
+          _localIP = wifiIP;
+        });
+      } else {
+        // fallback to platform-specific logic
+        String? ipAddress = await _getPlatformSpecificIP();
+        setState(() {
+          _localIP = ipAddress ?? "Could not fetch local IP.";
+        });
+      }
+    } catch (e) {
+      setState(() {
+        _localIP = "Error: $e";
+      });
+    }
+  }
+
+  // Platform-specific logic to get the IP address
+  Future<String?> _getPlatformSpecificIP() async {
+    try {
+      if (Platform.isLinux || Platform.isMacOS) {
+        // Use ifconfig or ip command on Unix-based systems
+        var result = await Process.run('ifconfig', []);
+        if (result.exitCode == 0) {
+          // Extract inet address (IPv4) using regex
+          RegExp inetRegex = RegExp(r'inet (\d+\.\d+\.\d+\.\d+)');
+          var match = inetRegex.firstMatch(result.stdout);
+          if (match != null) return match.group(1);
+        }
+      } else if (Platform.isWindows) {
+        var result = await Process.run('ipconfig', []);
+        if (result.exitCode == 0) {
+          // Extract IPv4 address using regex
+          RegExp ipv4Regex = RegExp(r'IPv4 Address[^\d]*(\d+\.\d+\.\d+\.\d+)');
+          var match = ipv4Regex.firstMatch(result.stdout);
+          if (match != null) return match.group(1);
+        }
+      }
+    } catch (e) {
+      // print("Error fetching IP address: $e");
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(title: const Text('Local IP Finder')),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // const Divider(),
+            Container(
+              margin: const EdgeInsets.only(bottom: 16.0),
+              child: Text(
+                'Your Local IP is: \n$_localIP:3000',
+                style: const TextStyle(fontSize: 24),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(bottom: 16.0),
+              child: QrImageView(
+                data: '$_localIP:3000',
+                version: QrVersions.auto,
+                size: 200.0,
+              ),
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
