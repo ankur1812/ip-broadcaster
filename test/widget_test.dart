e96e81e3af59933a2ac7c4c0667165370aae24c9
ankur1812 <ankur.anmol1992@gmail.com>
2025-01-07 20:00:43 +0530

    update app name globally

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:broadcast_my_ip/main.dart';
+import 'package:ip_relay/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
